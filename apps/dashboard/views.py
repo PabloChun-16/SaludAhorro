90db7b76d75700e481e0efd6eecac68f5f87634d
Pablo <javierchun@gmail.com>
2025-08-31 10:06:39 -0600

    Prueba de git en dashboard

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("dashboard OK")
+    return HttpResponse("dashboard OK - Prueba GIT")
