ca5e9c6492bca97ea7dda26b85565089c3028eff
Jose Daniel <jrecincoys@miumg.edu.gt>
2025-08-31 21:55:56 -0600

    Configuracion de PostgreSQL y primer modelo

--- apps/mantenimiento/models.py
+++ apps/mantenimiento/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Roles(models.Model):
+    nombre_rol = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.nombre_rol
+    
+    class Meta:
+        verbose_name = 'Rol'
+        verbose_name_plural = 'Roles'
