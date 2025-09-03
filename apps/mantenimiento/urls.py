14b66fc148e2a2b714f64aaa7e57569d4af880fa
Sebas <sebasmoralesj2003@gmail.com>
2025-09-02 19:46:47 -0600

    Creacion De Modulo Mantenimiento

--- apps/mantenimiento/urls.py
+++ apps/mantenimiento/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = "mantenimiento"
+
 urlpatterns = [
-    path('', views.index, name='mantenimiento_index'),
+    path("", views.index, name="index"),  # <-- /mantenimiento/
+    path("usuarios/", views.usuarios, name="usuarios"),
+    path("roles/", views.roles, name="roles"),
+    path("laboratorios/", views.laboratorios, name="laboratorios"),
+    path("presentaciones/", views.presentaciones, name="presentaciones"),
+    path("unidades_medida/", views.unidades_medida, name="unidades_medida"),
+    path("condiciones_almacenamiento/", views.condiciones_almacenamiento, name="condiciones_almacenamiento"),
 ]
--- apps/mantenimiento/views.py
+++ apps/mantenimiento/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse   
+from django.shortcuts import render, HttpResponse
+
 
 def index(request):
-    return HttpResponse("Bienvenido al módulo de mantenimiento")
+    return render(request, "mantenimiento/index.html")
+
+
+def usuarios(request):
+    return HttpResponse("Gestión de Usuarios")
+
+def roles(request):
+    return HttpResponse("Gestión de Roles")
+
+def laboratorios(request):
+    return HttpResponse("Gestión de Laboratorios")
+
+def presentaciones(request):
+    return HttpResponse("Gestión de Presentaciones")
+
+def unidades_medida(request):
+    return HttpResponse("Gestión de Unidades de Medida")
+
+def condiciones_almacenamiento(request):
+    return HttpResponse("Gestión de Condiciones de Almacenamiento")
