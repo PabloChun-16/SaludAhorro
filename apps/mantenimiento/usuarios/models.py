ef2d973ef1d4291f39131dccca37d1d493871166
Pablo <javierchun@gmail.com>
2025-09-05 22:47:40 -0600

    Corrección Usuarios - Hash

--- apps/mantenimiento/usuarios/models.py
+++ apps/mantenimiento/usuarios/models.py
@@ -1,15 +1,25 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
 
 class Usuario(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     correo_electronico = models.CharField(max_length=255, unique=True)
-    password_hash = models.CharField(max_length=255)
+    # 128 es suficiente para pbkdf2 sha256
+    password_hash = models.CharField(max_length=128)
+
     id_rol = models.ForeignKey(
-        "mantenimiento.Roles",  # ✅ referencia por string a la app y modelo
+        "mantenimiento.Roles",
         on_delete=models.CASCADE,
         db_column="id_rol_id"
     )
+
+    # === Helpers de password ===
+    def set_password(self, raw_password: str) -> None:
+        self.password_hash = make_password(raw_password)
+
+    def check_password(self, raw_password: str) -> bool:
+        return check_password(raw_password, self.password_hash)
 
     def __str__(self):
         return f"{self.nombre} {self.apellido}"
@@ -18,4 +28,3 @@ class Usuario(models.Model):
         db_table = "mantenimiento_usuarios"
         verbose_name = "Usuario"
         verbose_name_plural = "Usuarios"
-
