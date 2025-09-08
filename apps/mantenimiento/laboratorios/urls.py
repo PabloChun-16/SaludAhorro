c1f16be63381c591a74471785071ca9d5c5b0e06
Yoselin-A <yalvarezr2@miumg.edu.gt>
2025-09-07 20:06:07 -0600

    Resolver conflicto en laboratorios/urls.py

--- apps/mantenimiento/laboratorios/urls.py
+++ apps/mantenimiento/laboratorios/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-<<<<<<< HEAD
 app_name = "laboratorios"
 
 urlpatterns = [
@@ -11,11 +10,3 @@ urlpatterns = [
     path("<int:pk>/editar/", views.editar_laboratorio, name="editar_laboratorio"),
     path("<int:pk>/eliminar/", views.eliminar_laboratorio, name="eliminar_laboratorio"),
 ]
-=======
-app_name = "mantenimiento_laboratorios"
-
-urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("lista/", views.LaboratorioListView.as_view(), name="lista"),
-]
->>>>>>> 8f1bf632157c62fc82b9665437497b376869f702
