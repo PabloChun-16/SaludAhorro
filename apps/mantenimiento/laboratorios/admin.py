db4c5c64139956b5eb006f0bf1bd7735fb8124a8
Yoselin-A <yalvarezr2@miumg.edu.gt>
2025-09-07 20:01:38 -0600

    Resuelvo conflicto en admin.py

--- apps/mantenimiento/laboratorios/admin.py
+++ apps/mantenimiento/laboratorios/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
 
-<<<<<<< HEAD
 # El modelo Laboratorio ya se registra en apps/mantenimiento/admin.py
 # Aquí no es necesario volver a registrarlo.
-=======
-# Register your models here.
->>>>>>> 8f1bf632157c62fc82b9665437497b376869f702
