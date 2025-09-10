99f12b1fc3bf116f134d2f3dd7128acd592cce1e
Pablo <javierchun@gmail.com>
2025-09-09 22:54:07 -0600

    Corrección URL Módulo de Presentaciones, pendiente confirmación.

--- apps/mantenimiento/urls.py
+++ apps/mantenimiento/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("laboratorios/", include(("apps.mantenimiento.laboratorios.urls", "laboratorios"), namespace="mantenimiento_laboratorios")),
     path("roles/", include(("apps.mantenimiento.roles.urls", "roles"), namespace="mantenimiento_roles")),
     path("usuarios/", include(("apps.mantenimiento.usuarios.urls", "usuarios"), namespace="mantenimiento_usuarios")),
-    path("presentaciones/", include("apps.mantenimiento.presentaciones.urls")),
+    path("presentaciones/", include(("apps.mantenimiento.presentaciones.urls", "presentaciones"), namespace="mantenimiento_presentaciones")),
     path("unidadesmedida/", include(("apps.mantenimiento.unidadesmedida.urls", "unidadesmedida"), namespace="unidadesmedida")),
     path("condicionesalmacenamiento/", include(("apps.mantenimiento.condicionesalmacenamiento.urls", "condicionesalmacenamiento"), namespace="mantenimiento_condicionesalmacenamiento")),
 ]
