ab9cbcdcbeb9b62f22039eba3b53a216738da651
Pablo <javierchun@gmail.com>
2025-09-24 15:06:27 -0600

    Gráfica para STOCK (Stock Critico)

--- apps/inventario/stock/urls.py
+++ apps/inventario/stock/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("", views.stock_list, name="lista"),
     path("<int:pk>/consultar/", views.stock_detail, name="consultar"),
     path("exportar/pdf/", views.exportar_stock_pdf, name="exportar_pdf"),
+    path("reporte/stock-critico/", views.reporte_stock_critico, name="reporte_stock_critico"),
+
 ]
