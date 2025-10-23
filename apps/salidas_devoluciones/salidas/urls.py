43b9a45175fa05c5393c114d3e876c2e60d7c45b
Jose Daniel <jrecincoys@miumg.edu.gt>
2025-10-22 21:37:11 -0600

    Estados Salidas

--- apps/salidas_devoluciones/salidas/urls.py
+++ apps/salidas_devoluciones/salidas/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.venta_list, name="list"),                 # ✅ nombre correcto (sin “s”)
     path("crear/", views.venta_create, name="create"),
     path("detalle/<str:ref>/", views.venta_detail, name="detail"),
+     path("cancelar/<str:ref>/", views.venta_cancel, name="cancel"),   
 ]
